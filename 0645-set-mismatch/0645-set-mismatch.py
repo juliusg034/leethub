5e49fca8f27449b445a6803d50f06f956de0aee8
Julius Gutierrez <106943951+juliusg034@users.noreply.github.com>
2026-04-24 15:37:21 -0700

    0645-SET-MISMATCH Stats: Time: 11 ms (67.61%), Space: 21 MB (40.36%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.py
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -6,11 +6,12 @@ class Solution:
         for num in nums:
             if num not in check:
                 check.add(num)
-                continue
-            ans[0] = num
+            else:
+                ans[0] = num
 
         for i in range(1,len(nums)+1):
             if i not in check:
                 ans[1] = i
+                break
 
         return ans
