d0519622bfe493b402f74c02b9904d845fe0d8b7
Julius Gutierrez <106943951+juliusg034@users.noreply.github.com>
2026-06-03 12:57:10 -0700

    0252-MEETING-ROOMS Stats: Time: 0 ms (100%), Space: 2.8 MB (56.25%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.rs
+++ 0252-meeting-rooms/0252-meeting-rooms.rs
@@ -1,32 +1,18 @@
 use std::collections::HashMap;
 impl Solution {
-    pub fn can_attend_meetings(intervals: Vec<Vec<i32>>) -> bool {
+    pub fn can_attend_meetings(mut intervals: Vec<Vec<i32>>) -> bool {
 
-        /* Psuedo code:
+        intervals.sort_by_key(|interval| interval[0]);
 
-            checking meeting time start/end
-            check hashmap else add to hashmap
+        for i in 1..intervals.len() {
+            let prev_end = intervals[i-1][1];
+            let curr_start = intervals[i][0];
 
-            if in hashmap:
-                check if time conflicts:
-                    return false
-                else
-                    continue
-         */
-        
-        let mut map = HashMap::new();
-            for interval in intervals {
-                let start = interval[0];
-                let end = interval[1];
-
-                for num in start..end {
-                    if map.contains_key(&num) {
-                        return false
-                    }
-                    map.insert(num, true);
-                }
+            if curr_start < prev_end {
+                return false
             }
+        }
 
-            true
+        return true
     }
 }
